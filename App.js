60519f8fd3b3acdf129b87a22f669f7e0485fae3
Muhammad Shaheryar Haider <shaheryarjerry@gmail.com>
2021-11-07 13:26:48 +0500

    Bar and Pie Chart

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { VictoryChart, VictoryBar, VictoryTheme, VictoryPie } from 'victory-native'
+import data from './Data'
+
 
 export default function App() {
+  const Data = [
+    {
+      month: 1,
+      earning:546
+  },
+  {
+      month: 2,
+      earning:346
+  },
+  {
+      month: 3,
+      earning:646
+  },
+  {
+      month: 4,
+      earning:346
+  },
+  {
+      month: 5,
+      earning:345
+  },
+  {
+      month: 6,
+      earning:546
+  },
+  {
+      month: 7,
+      earning:326
+  }
+    
+  ]
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView >
+      <View style = {styles.container}>
+      <VictoryPie  animate theme={ VictoryTheme.material} data={ Data} x= "month" y = "earning" />
+      <Text> Bar Chart</Text>
+      
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryBar style={{
+                    data: {width: 10, fill: (data) => data.x === 1 ? "#d9d9d9" : "#2ba8de"
+                  },
+                    labels: {  fontSize: 10, fontFamily: "Whitney"}
+          }}
+            labels={({ datum }) => `${datum._y}`} animate data={Data} x="month" y="earning" />
+      </VictoryChart>
+      
+      </View>      
+      
+    </ScrollView>
   );
 }
 
---
+++ Data.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+        month: 1,
+        earning:546
+    },
+    {
+        month: 2,
+        earning:346
+    },
+    {
+        month: 3,
+        earning:646
+    },
+    {
+        month: 4,
+        earning:346
+    },
+    {
+        month: 5,
+        earning:345
+    },
+    {
+        month: 6,
+        earning:546
+    },
+    {
+        month: 7,
+        earning:326
+    },
+]
